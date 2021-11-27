99196e5d1404f5b9b29ab132a67a5377cc1b2fe6
Isamu Arimoto <isamu@to-kyo.to>
2021-11-27 16:08:50 +0900

    update

--- src/components/FriendlyEats.js
+++ src/components/FriendlyEats.js
@@ -107,6 +107,29 @@ export const data = {
   ],
 };
 
+export const priceOptions = ["$", "$$", "$$$", "$$$$"];
+export const sortOrderOptions = ["Rating", "Reviews"];
+
 export const getRandomItem = (items) => {
   return items[Math.floor(Math.random() * items.length)];
+};
+
+export const getStar = (rating) => {
+  const ret = [];
+  for (let r = 0; r < 5; r += 1) {
+    if (r < Math.floor(rating)) {
+      ret.push({ id: r, value: "star" });
+    } else {
+      ret.push({ id: r, value: "star_border" });
+    }
+  }
+  return ret;
+};
+
+export const getPrice = (price) => {
+  const ret = [];
+  for (let r = 0; r < price; r += 1) {
+    ret.push({ id: r, value: "$" });
+  }
+  return ret;
 };
